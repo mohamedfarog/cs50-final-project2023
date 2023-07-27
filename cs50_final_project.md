d0014b6fc778bdb46ceafa1eaa8e96dd15665c0e
unknown <m.farog@navcon.ae>
2023-07-27 11:24:36 +0400

    cs50

---
+++ cs50_final_project.md
@@ -0,0 +1,28 @@
+# Project title
+- CS50 Final Project <url> 
+
+# Description
+- CS50 Final Project is a web-based application using JavaScript, Python, Flask and SQL
+- on which a user can register a new account, login, view details, view home, update details, and logout.
+- the register page, a user is able to enter his or her information, including user name, email, and password.
+- then press register, which sends a post-reguest to save your date in date base.
+- the login page, a user uses the details that were provided earlier during registration (username, password) to login ino system.
+- the home page, a Logged in successfully  message is displayed with a header that says Welcome!! CS50 Home Page.
+- In the side bar, there are 4 buttons: home, user details, update info, and logout, so the user could switch through those pages.
+- the user details page, all the user's details are shown.
+- On the update page, a user is able to update information, for example, a password or email.
+- If the logout button is clicked, the system will logout, and the user must provide login details again to login into the system.
+- This project satisfies all the web application rules, including ( CURD ) POST, GET, UPDATE.
+- It uses user authentication and verification at both the front-end and back-end levels.
+
+
+
+# How to deploy and run
+- Create mysql database with the name cs50_final_project
+- Import the attached file cs50_final_project.sql to your database
+- Change the db connection params in app.py bassed on your db details
+- Open the project with your preferred IDE
+- Flask run
+- Browse to http://127.0.0.1:5000 
+- On login page switch to register page to create new account
+- Use the account details (username and password) to login
---
+++ cs50_final_project.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2023 at 12:43 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs50_final_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `organisation` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(250) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `postalcode` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `organisation`, `address`, `city`, `state`, `country`, `postalcode`) VALUES
+(1, 'cs50test', 'cs50test@20222', 'eldgel2000@gmail.com', 'cs50', 'alain/uae', 'alain', 'alain', 'United Arab Emirates', '54225');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
